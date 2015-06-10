c864a1835eaa80533f96b9385940185691ecd476
blackwhitehere <guner.stan@gmail.com>
2015-06-10 21:16:01 +0200

    3rd function
    Not yet returns table

---
+++ VS/Disambiguation/Sample/SpecialCharacterRemover.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static string SpecialCharacterRemover(string s)
+    {
+        Regex r = new Regex("(?:[^a-z0-9 ]|(?<=['\"])s)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+        return r.Replace(s, String.Empty);
+    }
+}
